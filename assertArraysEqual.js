c1f358d926b7caf743e8f3af479a4feb117668e8
porchsal <porchsal@yahoo.com>
2023-02-26 13:31:54 -0500

    assertArraysEqual.js eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,22 @@
-const eqArrays = function (array1, array2){
-    if (array1.length !== array2.length){
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-    }
+  }
 
-    for(var i = 0; i < array1.length; i++){
-        if ( array1[i] !== array2[i]){
-            return false;
-        }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-    return true;    
-}
+  }
+  return true;
+};
 
-const assertArraysEqual = function(array1, array2){
-    if (eqArrays(array1, array2) === true){
-        console.log(`Assertion Passed: [${array1}] === [${array2}]`);
-    } else {
-      console.log(`Assertion Failed: [${array1}] !== [${array2}]`);
-    }
-}
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
 
 assertArraysEqual([1, 3, 23, 5, 4], [1, 3, 23, 5, 4]);
