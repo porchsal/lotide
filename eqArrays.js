cfa2044bc4d1fe8ac2f705ff19d1795b7a52c0d5
porchsal <porchsal@yahoo.com>
2023-02-26 12:10:56 -0500

    eqArrays.js fixed

--- eqArrays.js
+++ eqArrays.js
@@ -11,13 +11,14 @@ const eqArrays = function (array1, array2){
     if (array1.length !== array2.length){
     return false;
     }
+
     for(var i = 0; i < array1.length; i++){
         if ( array1[i] !== array2[i]){
             return false;
         }
-    return true;
-    }    
+    }
+    return true;    
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));  
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(eqArrays([1, 2, 23, 5], [1, 2, 23, 5]));  
+console.log(assertEqual(eqArrays([1, 2, 23, 5], [1, 2, 23, 5]), true));
