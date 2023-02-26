3fa94cbf6b15163a62bcd1f99ad3f3031d53dd16
porchsal <porchsal@yahoo.com>
2023-02-26 13:30:44 -0500

    eqArrays.js eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,24 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
-    } else {
-      console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
-    }
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
   
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   }
 
-const eqArrays = function (array1, array2){
-    if (array1.length !== array2.length){
-    return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
+  }
+  return true;
+};
 
-    for(var i = 0; i < array1.length; i++){
-        if ( array1[i] !== array2[i]){
-            return false;
-        }
-    }
-    return true;    
-}
-
-console.log(eqArrays([1, 2, 23, 5], [1, 2, 23, 5]));  
+console.log(eqArrays([1, 2, 23, 5], [1, 2, 23, 5]));
 console.log(assertEqual(eqArrays([1, 2, 23, 5], [1, 2, 23, 5]), true));
