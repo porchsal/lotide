9254ac6b8755965b197d49554216fc553f067c84
porchsal <porchsal@yahoo.com>
2023-03-15 21:59:53 -0400

    headTest.js with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,18 @@
-//TEST CODE
-const assertEqual = require(`../assertEqual`)
-const head = require(`../head`);
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([4,6,7,8]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5,4,6,7,8]", () => {
+    assert.strictEqual(head([5,4,6,7,8]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
