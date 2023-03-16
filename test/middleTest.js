3b3c82cd432c78720178514b3e69ae2920cd2bb0
porchsal <porchsal@yahoo.com>
2023-03-15 21:33:54 -0400

    middleTest.js

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const eqArrays = require("../eqArrays");
+const middle = require("../middle");
+
+console.log(middle([1,2,3,4]));
+console.log(middle([1,2,3,4,5]));
+console.log(middle([1,2]));
