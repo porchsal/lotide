dfea75d07e4db502666972085e2ad78c050bba1b
porchsal <porchsal@yahoo.com>
2023-03-16 00:16:18 -0400

    findKeyByValue.js refactor

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
-    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const findKeyByValue = function(parameter, value) {
   for (const attribute in parameter) {
     if (parameter[attribute] === value) {
@@ -15,14 +7,5 @@ const findKeyByValue = function(parameter, value) {
   return undefined;
 };
 
+module.exports = findKeyByValue;
 
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-  
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
