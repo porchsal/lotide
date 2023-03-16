22989f9e9df37eb68cd7b90e654743a6221fbeef
porchsal <porchsal@yahoo.com>
2023-03-16 00:21:24 -0400

    eqObjectsTest.js

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,7 @@
+  
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
+  
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
