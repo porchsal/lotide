4a7a8de7673b93e24bf1c0ef03d429751efa36c3
porchsal <porchsal@yahoo.com>
2023-03-16 00:21:06 -0400

    findKeyByValueTest.js

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,10 @@
+const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+    
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+
