7ed778dbab999fe77be8c258c00215e4ef7b3575
porchsal <porchsal@yahoo.com>
2023-03-16 00:22:33 -0400

    countLettersTest.js

---
+++ test/countLettersTest.js
@@ -0,0 +1,5 @@
+let compare1 = "exiting news in town";
+let compare2 = "exiting news";
+console.log(countLetters(compare1));
+console.log(countLetters(compare2));
+console.log(assertEqual(countLetters(compare1), countLetters(compare2)));
