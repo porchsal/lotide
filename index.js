8047a33dbaf383b5a85a1be61e8facc65b6476d3
porchsal <porchsal@yahoo.com>
2023-03-16 00:14:59 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+//index.js
+
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const reverse = require('./reverse');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  reverse: reverse,
+  takeUntil: takeUntil,
+  without: without
+
+};
