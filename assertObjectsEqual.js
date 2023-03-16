c7d0b11ed2678a29fe08a37cb15e66d99bd4cd72
porchsal <porchsal@yahoo.com>
2023-03-16 00:18:03 -0400

    assertObjectsEqual.js refactor

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -33,8 +22,6 @@ const assertObjectsEqual = function(actual, expected) {
     return console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   }
 
-}
-  ;
-const a = { a: '1', b: 2 };
-const b = { b: 1, a: '1' };
-console.log(assertObjectsEqual(a,b));
+};
+
+module.exports = assertObjectsEqual;
