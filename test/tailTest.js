1ce3dd737a8ad899527219a901a0cbc98c63db5c
porchsal <porchsal@yahoo.com>
2023-03-15 21:06:45 -0400

    tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require("../assertEqual");
+
+const tail = require("../tail");
+
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+const empty = [];
+console.log(tail(empty));
