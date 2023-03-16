528a6a9f87dd996889895134400f98fbfce43fd8
porchsal <porchsal@yahoo.com>
2023-03-15 21:34:34 -0400

    middle.js refactor

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-    
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Assertion Failed: [${array1}] !== [${array2}]`);
-  }
-};
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
 
 const middle = function(array) {
   let middle = [];
@@ -33,9 +16,6 @@ const middle = function(array) {
     middle.push(array[mid]);
     return middle;
   }
+};
 
-}
-;
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,4,5]));
-console.log(middle([1,2]));
+module.exports = middle;
