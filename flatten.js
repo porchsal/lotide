818b571035d501ccaaece36406eecf208fc17b1e
porchsal <porchsal@yahoo.com>
2023-03-16 00:15:38 -0400

    flatten.js refactor

--- flatten.js
+++ flatten.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-  
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Assertion Failed: [${array1}] !== [${array2}]`);
-  }
-};
-
 const flatten = function(array) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -32,5 +11,5 @@ const flatten = function(array) {
   }
   return newArray;
 };
-  
-console.log(flatten([1, 2, 3, [4 , 5], 6, [7, 8]]));
+
+module.exports = flatten;
