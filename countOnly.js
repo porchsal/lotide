94ca8a05d4b1abde0d100c8ae5967eff5c3ca262
porchsal <porchsal@yahoo.com>
2023-03-18 00:38:31 -0400

    countOnly remove unused function

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
