0380a92c78e99df4007eebcf0b9b24caebb63ca5
porchsal <porchsal@yahoo.com>
2023-03-18 22:41:47 -0400

    assertEqualTest.js upload

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 2);
+assertEqual("Julian", "Julian");
+assertEqual("Julian", "Julia");
+assertEqual(45, 45);
