45a83f2b97969af8be22851fc9ec14ed17579446
porchsal <porchsal@yahoo.com>
2023-03-18 22:50:02 -0400

    countLettersTest.js upload

---
+++ test/countLettersTest.js
@@ -0,0 +1,7 @@
+const countLetters = require("../countLetters");
+const assertEqual = require("../assertEqual")
+let compare1 = "exiting news in town";
+let compare2 = "exiting news";
+console.log(countLetters(compare1));
+console.log(countLetters(compare2));
+//console.log(assertEqual(countLetters(compare1), countLetters(compare2)));
