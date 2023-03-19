1906650b9d93678ab295db99bdd81e3dd85f3d9c
porchsal <porchsal@yahoo.com>
2023-03-19 00:24:08 -0400

    eqObjects.js function fixed

--- eqObjects.js
+++ eqObjects.js
@@ -1,20 +1,22 @@
-const eqArrays = require(".");
+const eqArrays = require("./eqArrays");
+
 
 const eqObjects = function(object1, object2) {
-  const object1Array = Object.values(object1);
-  const object2Array = Object.values(object2);
+  const object1Array = Object.keys(object1);
+  const object2Array = Object.keys(object2);
   if (object1Array.length !== object2Array.length) {
     return false;
   } else {
-    for (const attribute in object1Array) {
+    for (const attribute of object1Array) {
       const value1 = object1[attribute];
       const value2 = object2[attribute];
       if (Array.isArray(value1) || Array.isArray(value2)) {
         if (!eqArrays(value1,value2)) {
           return false;
         }
+      } else if (value1 !== value2) {
+        return false;
       }
-      
     }
     return true;
   }
