33982e36968d9619a24c031b8d7995892afbc42d
porchsal <porchsal@yahoo.com>
2023-03-19 00:59:04 -0400

    takeUntilTest.js new test

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -7,5 +7,5 @@ console.log(results1);
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+const results2 = takeUntil(data2, x => x === ' ');
 console.log(results2);
