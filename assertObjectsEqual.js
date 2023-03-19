dce191ab3a9f405d7208d416e6fa40d7042acc86
porchsal <porchsal@yahoo.com>
2023-03-19 00:23:20 -0400

    assertObjectsEqual.js importing function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,13 @@
-const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  const actualArray = Object.keys(actual);
-  const expectedArray = Object.keys(expected);
-  if (actualArray.length !== expectedArray.length) {
+  if (!eqObjects(actual, expected)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   } else {
-    for (const key of actualArray) {
-      const value1 = actual[key];
-      const value2 = expected[key];
-      if (Array.isArray(value1) || Array.isArray(value2)) {
-        if (!eqArrays(value1,value2)) {
-          return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-        }
-      } else if (value1 !== value2) {
-        return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-      }
-    
-    }
     return console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   }
-
 };
 
 module.exports = assertObjectsEqual;
