3efe1c8469767f4c36d821f6a5051b74499a6b36
porchsal <porchsal@yahoo.com>
2023-03-18 23:14:11 -0400

    findKeyTest.js upload

---
+++ test/findKeyTest.js
@@ -0,0 +1,12 @@
+const findKey = require("../findKey");
+const assertEqual = require("../assertEqual")
+let value = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2); // => "noma"
+
+  assertEqual(value, "noma");
