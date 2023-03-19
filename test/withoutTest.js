14dd74bb9cbe1484c785c060bcc881cb598f23b7
porchsal <porchsal@yahoo.com>
2023-03-19 00:59:24 -0400

    withoutTest.js new test

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,4 +1,4 @@
 const without = require("../without")
 
-console.log(without([1, 2, 3, 4, 3],[2]));
+console.log(without([1, 2, 3, 4, 3, 5],[3, 4]));
 console.log(without(["a", "b", "c", "d", "e"],["b"]));
