cf0cefab9274068766044721b2d35e55c106eb0d
porchsal <porchsal@yahoo.com>
2023-03-19 00:57:23 -0400

    assertObjectsEqualTest.js new test

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,5 +1,5 @@
 const assertObjectsEqual = require("../assertObjectsEqual");
 
-const a = { a: '1', b: 2 };
+const a = { a: '1', b: 1 };
 const b = { b: 2, a: '1' };
 console.log(assertObjectsEqual(a,b));
