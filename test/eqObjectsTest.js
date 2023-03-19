0c9981d57c57374fb8e51a1e52a694316488b8b6
porchsal <porchsal@yahoo.com>
2023-03-19 00:57:58 -0400

    eqObjectsTest.js new test

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -7,3 +7,10 @@ console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
+
+console.log(eqObjects({a:1, b:2}, {b:1, a:1})); // false
+console.log(eqObjects({a:1, b:2}, {b:2, a:2})); // false
+console.log(eqObjects({a:1, b:[2,3]}, {a:1, b:[2,4]})); // false
+console.log(eqObjects({a:[1,2], b:2}, {a:[2,2], b:2})); // false
+console.log(eqObjects({a:1, b:[2,3]}, {a:2, b:[2,3]})); // false
+console.log(eqObjects({a:[1,2], b:2}, {a:[1,2], b:3})); // false
